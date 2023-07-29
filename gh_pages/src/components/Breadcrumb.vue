6ed28b2ded324582c812e7c17f7e3db92326f7ae
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-07-29 00:55:18 -0700

    update gh-pages

--- gh_pages/src/components/Breadcrumb.vue
+++ gh_pages/src/components/Breadcrumb.vue
@@ -1,15 +1,12 @@
 <template>
-    <ul class="breadcrumb">
-      <li
-        v-for="(breadcrumb, idx) in breadcrumbList"
-        :key="idx"
-        @click="routeTo(breadcrumb.path)"
-        :class="{'linked': !!breadcrumb.link}">
+  <ul class="breadcrumb">
+    <li v-for="(breadcrumb, idx) in breadcrumbList" :key="idx" @click="routeTo(breadcrumb.path)"
+      :class="{ 'linked': !!breadcrumb.link }">
 
-        <a href="javascript:void(0);">{{ breadcrumb.name }}</a>
+      <a href="javascript:void(0);">{{ breadcrumb.name }}</a>
 
-      </li>
-    </ul>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -17,20 +14,20 @@ import ConfigManager from '../services/configManager'
 
 export default {
   name: 'Breadcrumb',
-  data () {
+  data() {
     return {
       breadcrumbList: []
     }
   },
-  mounted () { this.updateList() },
-  watch: { '$route' () { this.updateList() } },
+  mounted() { this.updateList() },
+  watch: { '$route'() { this.updateList() } },
   methods: {
-    routeTo (targetPath) {
+    routeTo(targetPath) {
       // console.log('shall route to ', targetPath)
       this.$router.push(targetPath)
       // if (this.breadcrumbList[pRouteTo].link) this.$router.push(this.breadcrumbList[pRouteTo].link)
     },
-    updateList () {
+    updateList() {
       let currentPath = this.$router.currentRoute.path
       this.pageConfig = ConfigManager.getMetaById(currentPath)
 
@@ -43,38 +40,37 @@ export default {
 </script>
 
 <style scoped>
+ul.breadcrumb {
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  padding-top: 8px;
+  padding-bottom: 7px;
+  padding-left: 1.2rem;
+  margin: 0;
+  list-style: none;
+  background: #f8f8f8;
+  border-bottom: 1px solid #ddd;
+}
 
-  ul.breadcrumb {
-    position: sticky;
-    top: 0;
-    z-index: 1;
-    padding-top: 8px;
-    padding-bottom: 7px;
-    padding-left: 1.2rem;
-    margin: 0;
-    list-style: none;
-    background: #f8f8f8;
-    border-bottom: 1px solid #ddd;
-  }
+ul.breadcrumb li {
+  display: inline;
+  font-size: 0.9rem;
+}
 
-  ul.breadcrumb li {
-    display: inline;
-    font-size: 0.9rem;
-  }
+ul.breadcrumb li+li::before {
+  padding: 8px;
+  color: black;
+  content: "/\00a0";
+}
 
-  ul.breadcrumb li + li::before {
-    padding: 8px;
-    color: black;
-    content: "/\00a0";
-  }
+ul.breadcrumb li a {
+  color: #0275d8;
+  text-decoration: none;
+}
 
-  ul.breadcrumb li a {
-    color: #0275d8;
-    text-decoration: none;
-  }
-
-  ul.breadcrumb li a:hover {
-    color: #01447e;
-    text-decoration: underline;
-  }
+ul.breadcrumb li a:hover {
+  color: #001934;
+  text-decoration: underline;
+}
 </style>
--- gh_pages/src/components/Footer.vue
+++ gh_pages/src/components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
-    <section style="background-color:#ededed">
-        <div class="footer">
-            <b>Client Support Transformation</b> - Making our clients wildly successful
-        </div>
-    </section>
+  <section style="background-color:#ededed">
+    <div class="footer">
+      <b>Client Support Transformation</b> - Making our clients wildly successful
+    </div>
+  </section>
 </template>
 
 <script>
 
 export default {
   name: 'tools-and-resources',
-  data () {
+  data() {
     return {
     }
   }
